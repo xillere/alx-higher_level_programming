1338bef21c3855a8c5eaeb6300e6071236e7fc7d
Chideraa <chideraaozobu@gmail.com>
2023-10-05 11:45:47 -0700

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    num = len(sys.argv) - 1
+    if num == 0:
+        print("{} arguments.".format(num))
+    else:
+        print("{} arguments:".format(num))
+
+    if num > 0:
+        for i in range(num):
+            print("{}: {}".format(i, sys.argv[num + 1]))

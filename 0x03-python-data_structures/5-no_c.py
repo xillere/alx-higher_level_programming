73a2a3e7ab3040fed2d8993fbbba8dbdbb6aa0ad
Chideraa <chideraaozobu@gmail.com>
2023-10-09 17:19:23 -0700

    replace character list

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     another = my_string.copy()
     for i in range(len(my_string)):
-        if another[i] in 'Cc':
+        if (another[i] in 'C') or (another[i] in 'c'):
             another[i] = ''
     return another

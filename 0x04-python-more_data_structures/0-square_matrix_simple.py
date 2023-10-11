029f61c64bcdfb4c33510687780ebb220901eb76
Chideraa <chideraaozobu@gmail.com>
2023-10-11 15:05:38 -0700

    more

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ def square_matrix_simple(matrix=[]):
     another = []
     for col in matrix:
         res = list(map(lambda x: x**2, col))
-        another.append(result)
+        another.append(res)
     return another
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,5 @@
 def print_sorted_dictionary(a_dictionary):
     s = list(a_dictionary.keys())
     s.sort()
-    for i in keys:
+    for i in s:
         print("{}: {}".format(i, a_dictionary[i]))

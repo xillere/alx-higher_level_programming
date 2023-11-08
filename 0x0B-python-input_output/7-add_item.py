40fbc27ffc9a9bd4a3eb2261f8096c54e93d40e3
Chideraa <chideraaozobu@gmail.com>
2023-11-07 16:14:58 -0800

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
         __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        argus = load_from_json_file("add_item.json")
+        item = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        argus = []
-    argus.extend(sys.argv[1:])
-    save_to_json_file(argus, "add_item.json")
+        item = []
+    item.extend(sys.argv[1:])
+    save_to_json_file(item, "add_item.json")
